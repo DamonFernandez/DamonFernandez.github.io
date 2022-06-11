5797d01f07b59542025d6ff58c78b126a5461f32
DamonFernandez <damonfernandez04@gmail.com>
2022-06-11 15:15:07 -0400

    The first commit ever :)

---
+++ HTMl test 2 for javascript/index.js
@@ -0,0 +1,11 @@
+document.getElementById("myButton").onclick = function(){
+
+    const myCheckBox = document.getElementById("myCheckBox")
+    if(myCheckBox.checked){
+        alert("Good job Damon :)))) 👍");
+    }
+    else{
+        alert("... Damon... You Gotta do better than that bro, you only get one life");
+    }
+
+}
